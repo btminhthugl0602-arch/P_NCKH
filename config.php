958970955e98049474430b7a674d9bcc12f6381a
Thnhhuyn <nthanhhuyen191@gmail.com>
2026-02-14 14:01:41 +0700

    js

--- config.php
+++ config.php
@@ -15,8 +15,8 @@ const _DRIVER = 'mysql';
 const _DEBUG = true;
 
 //  Thiết lập host
-define('_HOST_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/NCKH2');
-define('_HOST_URL_TEMPLATES', 'http://' . $_SERVER['HTTP_HOST'] . '/NCKH2/template');
+define('_HOST_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/P_NCKH');
+define('_HOST_URL_TEMPLATES', _HOST_URL . '/template');
 
 //  Thiết lập path
 define('_PATH_URL', __DIR__);
--- template/layouts/index.php
+++ template/layouts/index.php
@@ -23,13 +23,13 @@ if (!defined('_AUTHEN')) {
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="<?php echo _HOST_URL_TEMPLATES ?>/assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo _HOST_URL_TEMPLATES ?>/assets/css/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?php echo _HOST_URL_TEMPLATES ?>/assets/css/aos.css" rel="stylesheet">
+  <link href="<?php echo _HOST_URL_TEMPLATES ?>/assets/css/swiper-bundle.min.css" rel="stylesheet">
 
   <!-- Main CSS File -->
-  <link href="assets/css/main.css" rel="stylesheet">
+  <link href="<?php echo _HOST_URL_TEMPLATES ?>/assets/css/main.css" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: Learner
@@ -421,14 +421,14 @@ if (!defined('_AUTHEN')) {
   <div id="preloader"></div>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="<?php echo _HOST_URL_TEMPLATES ?>/assets/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo _HOST_URL_TEMPLATES ?>/assets/js/validate.js"></script>
+  <script src="<?php echo _HOST_URL_TEMPLATES ?>/assets/js/aos.js"></script>
+  <script src="<?php echo _HOST_URL_TEMPLATES ?>/assets/js/purecounter_vanilla.js"></script>
+  <script src="<?php echo _HOST_URL_TEMPLATES ?>/assets/js/swiper-bundle.min.js"></script>
 
   <!-- Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script src="<?php echo _HOST_URL_TEMPLATES ?>/assets/js/main.js"></script>
 
 </body>
 
