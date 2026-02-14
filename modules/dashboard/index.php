512ad2ee6198cbf99fd8946c4b4bdf00d7fec924
Thnhhuyn <nthanhhuyen191@gmail.com>
2026-02-14 21:55:23 +0700

    navbar

--- modules/dashboard/index.php
+++ modules/dashboard/index.php
@@ -2,3 +2,15 @@
 if (!defined('_AUTHEN')) {
     die('Truy cập không hợp lệ');
 }
+// Kiểm tra đăng nhập
+if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
+  header("Location: " . _HOST_URL . "?module=auth&action=login");
+  exit();
+}
+$active_page = 'dashboard';
+$data = [
+  'page_title' => 'Dashboard'
+];
+// Include header
+layout('header', $data);
+?>
--- template/layouts/navbar.php
+++ template/layouts/navbar.php
@@ -3,7 +3,7 @@ if (!defined('_AUTHEN')) {
     die('Truy cập không hợp lệ');
 }
 ?>
-<body class="enroll-page">
+<body>
 
   <header id="header" class="header d-flex align-items-center sticky-top">
     <div class="container-fluid container-xl position-relative d-flex align-items-center">
@@ -11,18 +11,16 @@ if (!defined('_AUTHEN')) {
       <a href="index.html" class="logo d-flex align-items-center me-auto">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <!-- <img src="assets/img/logo.webp" alt=""> -->
-        <h1 class="sitename">Learner</h1>
+        <h1 class="sitename">NCKH</h1>
       </a>
 
       <nav id="navmenu" class="navmenu">
         <ul>
           <li><a href="index.html">Home</a></li>
-          <li><a href="about.html">About</a></li>
-          <li><a href="courses.html">Courses</a></li>
-          <li><a href="instructors.html">Instructors</a></li>
-          <li><a href="pricing.html">Pricing</a></li>
-          <li><a href="blog.html">Blog</a></li>
-          <li class="dropdown"><a href="#"><span>More Pages</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+          <li><a href="about.html">Tổng quan</a></li>
+          <li><a href="events.html">Sự kiện</a></li>
+          <li><a href="instructors.html">Người dùng</a></li>
+          <li class="dropdown"><a href="#"><span>Quản trị hệ thống</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
             <ul>
               <li><a href="course-details.html">Course Details</a></li>
               <li><a href="instructor-profile.html">Instructor Profile</a></li>
@@ -33,30 +31,10 @@ if (!defined('_AUTHEN')) {
               <li><a href="404.html">404</a></li>
             </ul>
           </li>
-
-          <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-            <ul>
-              <li><a href="#">Dropdown 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-                <ul>
-                  <li><a href="#">Deep Dropdown 1</a></li>
-                  <li><a href="#">Deep Dropdown 2</a></li>
-                  <li><a href="#">Deep Dropdown 3</a></li>
-                  <li><a href="#">Deep Dropdown 4</a></li>
-                  <li><a href="#">Deep Dropdown 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Dropdown 2</a></li>
-              <li><a href="#">Dropdown 3</a></li>
-              <li><a href="#">Dropdown 4</a></li>
-            </ul>
-          </li>
-          <li><a href="contact.html">Contact</a></li>
-        </ul>
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
       </nav>
 
-      <a class="btn-getstarted" href="enroll.html">Enroll Now</a>
+      <a class="btn-getstarted" href="enroll.html">Đăng nhập</a>
 
     </div>
   </header>
