ada35c46d7caf96057205b9be58832f91aff89b2
Xxis9706 <datcaofashion@gmail.com>
2026-02-15 16:26:43 +0700

    reset1-delete

--- modules/event/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (!defined('_AUTHEN')) {
-    die('Truy cập không hợp lệ');
-}
-echo "Đây là trang sự kiện";
--- modules/users/list.php
+++
@@ -1,4 +0,0 @@
-<?php
-if (!defined('_AUTHEN')) {
-  die('Truy cập không hợp lệ');
-}
