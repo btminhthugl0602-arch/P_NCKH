7328e02d30fe2a5f9588150b063589a9d8d6be73
Xxis9706 <datcaofashion@gmail.com>
2026-02-22 23:19:58 +0700

    update cấu hình quyền

--- modules/functions/db_connect.php
+++ modules/functions/db_connect.php
@@ -1,14 +1,13 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "NCKH";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "NCKH";
 
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
-    mysqli_set_charset($conn, 'utf8mb4');
-?>
+mysqli_set_charset($conn, 'utf8mb4');
